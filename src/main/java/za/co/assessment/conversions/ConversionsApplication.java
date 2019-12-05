ace165c3ee97904df4e64a3c6db64837984d9e12
brilliantmashanda <masebenza.bm@gmail.com>
2019-12-05 11:11:17 +0200

    Assessment

--- src/main/java/za/co/assessment/conversions/ConversionsApplication.java
+++ src/main/java/za/co/assessment/conversions/ConversionsApplication.java
@@ -2,12 +2,17 @@ package za.co.assessment.conversions;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ConversionsApplication {
-
-	public static void main(String[] args) {
+public class ConversionsApplication extends SpringBootServletInitializer {
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(ConversionsApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ConversionsApplication.class);
+	}
 }
